cb3c5ea70ccf26b663c3722aec895d103b3e375d
BhavikPawar29 <bhavikdeepawar007@gmail.com>
2026-03-21 11:15:27 +0530

    Add Redis TLS and password support

--- src/db/redis.py
+++ src/db/redis.py
@@ -1,14 +1,16 @@
 import redis
 import os
-#from src.core.settings import settings
 
 REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
 REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
-
+REDIS_PASSWORD = os.getenv("REDIS_PASSWORD", None)
+REDIS_TLS = os.getenv("REDIS_TLS", "false").lower() == "true"
 
 redis_client = redis.Redis(
     host=REDIS_HOST,
     port=REDIS_PORT,
+    password=REDIS_PASSWORD,
     db=0,
-    decode_responses=True
+    decode_responses=True,
+    ssl=REDIS_TLS
 )
